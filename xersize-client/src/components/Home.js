af6135b0dfeb4ebbb60c7457c7254ff22cf56e63
Raiden Skala <trsraiden@gmail.com>
2021-01-20 13:33:11 -0500

    Remove images for heroku

--- xersize-client/src/components/Home.js
+++ xersize-client/src/components/Home.js
@@ -8,17 +8,17 @@ export default function Home(props) {
             <div className='appSection row'>
                 <div className='homeSectionDiv col-sm-4'>
                     <h2 className='markerSection' >Sessions</h2>
-                    <i className="homeIcon fas fa-chart-line"></i>
+                    {/* <i className="homeIcon fas fa-chart-line"></i> */}
                     <p className='robotoSection' >Track your progress to make sure you're getting results.</p>
                 </div>
                 <div className='homeSectionDiv col-sm-4'>
                     <h2 className='markerSection' >Routines</h2>
-                    <i className="homeIcon far fa-clipboard"></i>
+                    {/* <i className="homeIcon far fa-clipboard"></i> */}
                     <p className='robotoSection' >Discover routines created by other users or create your own!</p>
                 </div>
                 <div className='homeSectionDiv col-sm-4'>
                     <h2 className='markerSection' >Manage</h2>
-                    <i className="homeIcon fas fa-users-cog"></i>
+                    {/* <i className="homeIcon fas fa-users-cog"></i> */}
                     <p className='robotoSection' >Change your password or delete your account.</p>
                 </div>
             </div>
